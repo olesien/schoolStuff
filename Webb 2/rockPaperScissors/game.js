3a200db12ec0c68113902f3b1ee228cbe2f630e0
olesien <olesien2@gmail.com>
2019-11-11 12:38:37 +0100

    Update game.js

--- Webb 2/rockPaperScissors/game.js
+++ Webb 2/rockPaperScissors/game.js
@@ -24,12 +24,6 @@ Scissors.addEventListener('click', function () {
 
 });
 
-function reload() {
-    userScore = 0;
-    cpuScore = 0;
-    window.location.reload(true);
-}
-
 let isPlaying = true;
 
 function game(userChoice) {
@@ -51,53 +45,59 @@ function game(userChoice) {
                     console.log('Error Rock');
                     break;
                 }
-            case 'Paper':
-                if (cpuChoice == 'Paper') {
-                    status.innerHTML = 'Nobody Wins'
-                } else if (cpuChoice == 'Scissors') {
-                    status.innerHTML = 'You lose';
-                    cpuScore++;
-                } else if (cpuChoice == 'Rock') {
-                    status.innerHTML = 'You win';
-                    userScore++;
-                } else {
-                    console.log('Error Paper');
-                }
-                break;
+                case 'Paper':
+                    if (cpuChoice == 'Paper') {
+                        status.innerHTML = 'Nobody Wins'
+                    } else if (cpuChoice == 'Scissors') {
+                        status.innerHTML = 'You lose';
+                        cpuScore++;
+                    } else if (cpuChoice == 'Rock') {
+                        status.innerHTML = 'You win';
+                        userScore++;
+                    } else {
+                        console.log('Error Paper');
+                    }
+                    break;
 
-            case 'Scissors':
-                if (cpuChoice == 'Scissors') {
-                    status.innerHTML = 'Nobody Wins';
-                } else if (cpuChoice == 'Rock') {
-                    status.innerHTML = 'You lose';
-                    cpuScore++;
-                } else if (cpuChoice == 'Paper') {
-                    status.innerHTML = 'You win';
-                    userScore++;
-                } else {
-                    console.log('Error Scissors');
-                }
-                break;
-            default:
-                console.log('Error Invalid');
-                break;
+                case 'Scissors':
+                    if (cpuChoice == 'Scissors') {
+                        status.innerHTML = 'Nobody Wins';
+                    } else if (cpuChoice == 'Rock') {
+                        status.innerHTML = 'You lose';
+                        cpuScore++;
+                    } else if (cpuChoice == 'Paper') {
+                        status.innerHTML = 'You win';
+                        userScore++;
+                    } else {
+                        console.log('Error Scissors');
+                    }
+                    break;
+                default:
+                    console.log('Error Invalid');
+                    break;
         }
         score.innerHTML = ('You have ' + userScore + ' while the computer has ' + cpuScore)
         if (userScore == 5) {
             status.innerHTML = 'You win the game!'
             isPlaying = false;
+            restart();
         } else if (cpuScore == 5) {
             status.innerHTML = 'You lose the game!'
             isPlaying = false;
-        }
-    } else {
-        let btn = document.createElement("BUTTON");
-        btn.innerHTML = "Restart";
-        btn.addEventListener('click', function () {
             restart();
+        }
 
-        });
+        function restart() {
+            var btn = document.createElement("BUTTON");
+            btn.innerHTML = "Restart";
+            document.body.appendChild(btn);
+            btn.addEventListener('click', function () {
+                userScore = 0;
+                cpuScore = 0;
+                window.location.reload(true);
 
-
+            });
+        }
     }
+
 }
