d0208ff76e7c8e0150aa933dc67e57db122aba83
olesien <olesien2@gmail.com>
2019-11-14 11:34:47 +0100

    Update Program.cs

--- ConsoleApp2/ConsoleApp2/Program.cs
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -10,32 +10,29 @@ namespace ConsoleApp2
     {
         static void Main(string[] args)
         {
-            int zero = 0;
-            int one = 0;
-            int two = 0;
-            int three = 0;
-            int four = 0;
-            int five = 0;
-            int six = 0;
-            int seven = 0;
-            int eight = 0;
-            int nine = 0;
+            int[] array = new int[] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+            //array[0] += 1;
+            //Console.WriteLine(array[0]);
+
             Console.Write("enter N:");
             int n = int.Parse(Console.ReadLine());
 
             for (int i = 1; i <= n; i += 2)
             {
                 //Console.WriteLine("yes" + i.ToString().Length);
-                for (int it = 1; it <= i.ToString().Length; it++)
+                for (int iteration = 0; iteration <= i.ToString().Length-1; iteration++)
                 {
                     string tempi = i.ToString();
-                    Console.WriteLine(tempi[(it - 1)]);
+                    Console.WriteLine(tempi[iteration]);
+                    //Console.WriteLine(tempi[iteration]);
+                    //array[tempi[(it - 1)] - 1] += 1;
                     //Console.WriteLine(i.ToString()[it-1]);
                     //Console.WriteLine(": " + it);
                 }
                 //Console.WriteLine(i);
             }
-            Console.WriteLine(zero + " " + one + " " + two + " " + three + " " + four + " " + five + " " + six + " " + seven + " " + eight + " " + nine);
+            //Console.WriteLine(zero + " " + one + " " + two + " " + three + " " + four + " " + five + " " + six + " " + seven + " " + eight + " " + nine);
+            //Console.WriteLine(array[0]);
             Console.ReadKey();
 
 
