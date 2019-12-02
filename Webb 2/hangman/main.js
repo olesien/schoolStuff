bbbd42b9ffac052157ba6c4213ebcbf400ebeed4
olesien <olesien2@gmail.com>
2019-12-02 11:38:25 +0100

    a

--- Webb 2/hangman/main.js
+++ Webb 2/hangman/main.js
@@ -2,10 +2,11 @@ let words = ['another', 'test'];
 let input = document.querySelector('#input');
 let btn = document.querySelector('#btn');
 let newWord = document.querySelector('#newWord');
+let guessed = "0123456789";
 
 current = words[0] //fix this later so you can change
 for (let i = 0; i < current.length; i++) {
-    console.log("yes")
+    //console.log("yes")
     var para = document.createElement("p");
     var iString = 'p' + i.toString();
     para.className = iString;
@@ -21,19 +22,44 @@ for (let i = 0; i < current.length; i++) {
     element.insertBefore(para, child); //add class but too lazy
 }
 
-btn.addEventListener("click", myFunction() {
+//console.log(current);
+btn.addEventListener("click", function () {
     //here i want to compare input with every char in the list
-    let count = 0;
-    for (let i = 0; i < current.length; i++) {
-        if (input == current[i]) {
-            let str = document.querySelector('p' + i.toString());
-            str.innerHTML = current[i];
-            console.log("AYE");
-            count++;
-        }
-        if (count = 0) {
-            console.log("no");
+    //console.log(input.value);
+    //console.log("clicked");
+    //console.log(current.length);
+    run = true;
+    for (let ii = 0; ii < guessed.length; ii++) {
+        if (input.value == guessed[ii]) {
+            run = false;
         }
     }
+    if (run) {
+        let count = 0;
+        //console.log(current.length);
+        for (let i = 0; i < current.length; i++) {
+            //console.log("ree");
+
+            if (input.value == current[i]) {
+                console.log("yeah");
+                let str = document.querySelector('.p' + i.toString());
+                console.log('.p' + i.toString());
+
+                str.innerHTML = current[i];
+                //console.log(current[i]);
+                //console.log("AYE");
+                count++;
+            }
+            if (i == current.length - 1 && count == 0) {
+                console.log("no");
+            }
+            guessed += input.value;
+
+        }
+        guessed += input.value;
+    } else {
+        console.log("already guessed that word!");
+    }
+
 });
 //for loop here to iteration and check if input == current array[0]
