d1cc45f9865cc76d7683a5de2413c5184b2c3d57
olesien <olesien2@gmail.com>
2019-12-04 14:02:02 +0100

    Update main.js

--- Webb 2/hangman/main.js
+++ Webb 2/hangman/main.js
@@ -22,12 +22,8 @@ for (let i = 0; i < current.length; i++) {
     element.insertBefore(para, child); //add class but too lazy
 }
 
-//console.log(current);
 btn.addEventListener("click", function () {
     //here i want to compare input with every char in the list
-    //console.log(input.value);
-    //console.log("clicked");
-    //console.log(current.length);
     run = true;
     for (let ii = 0; ii < guessed.length; ii++) {
         if (input.value == guessed[ii]) {
@@ -36,9 +32,7 @@ btn.addEventListener("click", function () {
     }
     if (run) {
         let count = 0;
-        //console.log(current.length);
         for (let i = 0; i < current.length; i++) {
-            //console.log("ree");
 
             if (input.value == current[i]) {
                 console.log("yeah");
@@ -46,9 +40,11 @@ btn.addEventListener("click", function () {
                 console.log('.p' + i.toString());
 
                 str.innerHTML = current[i];
-                //console.log(current[i]);
-                //console.log("AYE");
                 count++;
+                //BELOW I WANT TO CHECK IF WON
+                //---
+
+                //---
             }
             if (i == current.length - 1 && count == 0) {
                 console.log("no");
@@ -60,6 +56,11 @@ btn.addEventListener("click", function () {
     } else {
         console.log("already guessed that word!");
     }
+
+});
+
+//below is for the new word button
+newWord.addEventListener("click", function () {
 
 });
 //for loop here to iteration and check if input == current array[0]
